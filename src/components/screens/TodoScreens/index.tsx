b87831c2b553bcbf2bd33869f4bcea0fe79a9e7c
nisitin <seiya110307@icloud.com>
2022-04-09 14:49:37 +0900

    fix: TodoScreensの作成

---
+++ src/components/screens/TodoScreens/index.tsx
@@ -0,0 +1,14 @@
+import { Button } from './components/ Button'
+import { Form } from './components/Form'
+import { List } from './components/List'
+
+export const TodoScreen = () => {
+  return (
+    <div>
+      <h1>Todoリスト</h1>
+      <Form />
+      <Button />
+      <List />
+    </div>
+  )
+}
