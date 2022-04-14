34b043a45cb8a4a1815d79dcc3b062f75b0b91b6
nisitin <seiya110307@icloud.com>
2022-04-14 22:06:18 +0900

    feat: hooksにstateを定義

--- src/components/screens/TodoScreens/components/Form.tsx
+++ src/components/screens/TodoScreens/components/Form.tsx
@@ -1,8 +1,14 @@
+import { HtmlProps } from 'next/dist/shared/lib/html-context'
+import { ChangeEvent, useState } from 'react'
+import { useTodoScreenState } from './hooks'
+
 export const Form = () => {
+  const { name, onChangeForm } = useTodoScreenState()
+
   return (
     <div>
       <form>
-        <input placeholder="タスク名" />
+        <input placeholder="タスク名" value={name} onChange={onChangeForm} />
       </form>
     </div>
   )
